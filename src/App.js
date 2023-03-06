daad7952fcd8593ee6929166baf8377a219514ce
waqasarshad225 <107762146+waqasarshad225@users.noreply.github.com>
2023-03-06 18:13:08 +0500

    TODO APP
    todo app assingment for using javascript , html , css , with react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import TodoList from './TodoList';
+
+
+// Todo list
+
+function App() {
+
+   return(
+    <div className = 'app'>
+      {/* <Todo/> */}
+      <TodoList/>
+    </div>
+   );
+}
+
+export default App;
